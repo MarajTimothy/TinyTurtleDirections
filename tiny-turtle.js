6079e29203c58ebe7c621ca5763d354f5d076d03
Timothy <timothy.t.c.maraj@gmail.com>
2017-04-03 22:46:16 +0000

    21 thavage

---
+++ tiny-turtle.js
@@ -0,0 +1,54 @@
+/* global forward*/
+/* global right */
+/* global TinyTurtle */
+
+TinyTurtle.apply(window);
+function moana(){
+    for(var i =0; i<2000; i= i+ 1){
+        forward(40);
+        right(90);
+        forward(30);
+        right(20)
+    }
+    
+    };
+    
+   
+
+function part(){
+    for(var i =0; i<2000; i= i+ 1){
+        forward(40);
+        right(90);
+        forward(30);
+        right(70)
+    }
+    
+    };
+    part()
+ 
+
+
+function part2(){
+    for(var i =0; i<200; i= i+ 1){
+        forward(40);
+        right(90);
+        forward(30);
+        right(17)
+    }
+    
+    };
+    
+part2()
+
+
+for(var i =0;i<40; i++){
+    forward(100)
+part2()
+}
+
+function sqaured(){
+    
+    forward(20)
+    right(90)
+    
+}
